ac13f187b05731d294264f433fd0b5148c1bc931
luck4you <31469918+Luuk-Ruigendijk@users.noreply.github.com>
2021-04-09 15:53:13 +0200

    removed demo migrations, finished order migration, created start for ordercontroller

---
+++ database/migrations/2021_04_08_115114_create_order_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('orderId')->unsigned();
+            $table->foreign('orderId')->references('id')->on('orders')->onDelete('cascade')->onUpdate('cascade');
+            $table->bigInteger('productId')->unsigned();
+            $table->foreign('productId')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
+            $table->double('amount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+}
